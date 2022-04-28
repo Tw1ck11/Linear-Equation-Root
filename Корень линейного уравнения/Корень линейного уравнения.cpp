8d4689b0e094cce49abbabb8ddc55a843086a4f3
Tw1ck11 <qwentyn2222222222@gmail.com>
2022-04-28 22:34:38 +0200

    Linear Equation Root

---
+++ Корень линейного уравнения/Корень линейного уравнения.cpp
@@ -0,0 +1,21 @@
+﻿// Корень линейного уравнения.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, x;
+
+    cout << "This program calculates the root of the linear equation.\nThe linear equation has the form 'ax + b = 0'.\nEnter coefficients a and b:" << endl;
+
+    cout << "Coefficient a = ";
+    cin >> a;
+
+    cout << "Coefficient b = ";
+    cin >> b;
+
+    x = -b / a;
+    cout << "x = " << x << endl;
+}
